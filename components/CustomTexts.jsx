e75d9ec9b31f355b26fc1044f492e00892da7a6e
sriank123 <141177387+sriank123@users.noreply.github.com>
2024-05-01 20:49:47 +0530

    Your commit message here

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,14 +1,19 @@
-'use client';
-
+// Import React
+import React from 'react';
+// Import framer-motion
 import { motion } from 'framer-motion';
+// Import animation variants
 import { textContainer, textVariant2 } from '../utils/motion';
 
+// TypingText Component
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
+    {/* Use Array.from to convert the string into an array */}
     {Array.from(title).map((letter, index) => (
+      // Use motion.span for animated span elements
       <motion.span variants={textVariant2} key={index}>
         {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
@@ -16,6 +21,7 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
+// TitleText Component
 export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
